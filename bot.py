c26da09689287b52b16446611a29ec78fbd97eba
technicalrambortdj-web <technicalrambortdj@gmail.com>
2026-07-20 15:02:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,14 @@
 import os
 import pyrogram
 
+import nntplib
+import time
+
+# Sync time tolerance offset
+from pyrogram.session import session
+session.Session.MAX_TIME_DIFF = 86400
+
+
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
